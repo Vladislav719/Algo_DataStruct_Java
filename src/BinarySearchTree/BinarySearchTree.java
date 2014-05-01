a6a1b56d17df70de4d27c278bf114c0462078dcd
Vladislav Baimurzin <baimurzin.719@gmail.com>
2014-05-01 19:17:12 +0400

    adding binary tree search structure

---
+++ src/BinarySearchTree/BinarySearchTree.java
@@ -0,0 +1,12 @@
+package BinarySearchTree;
+
+/**
+ * Created by vladislav on 01.05.14.
+ */
+public class BinarySearchTree {
+
+    static class Node{
+        int key;
+        int value
+    }
+}
